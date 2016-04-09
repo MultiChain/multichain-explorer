ad1f48106aa6436634b0f13a03b76853d44b93ff
Simon <simon@bitcartel.com>
2016-04-09 15:51:37 -0700

    Reduce home page refresh interval from 60 secs to 30 secs.

--- Mce/abe.py
+++ Mce/abe.py
@@ -497,7 +497,7 @@ class Abe:
                     '}, ', mempool_refresh_interval_ms, ');'
                     '});'
                     '</script>']
-        page_refresh_interval_secs = 60
+        page_refresh_interval_secs = 30
         myheader += ['<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />',
                      '<meta http-equiv="Pragma" content="no-cache" />',
                      '<meta http-equiv="Expires" content="0" />']
